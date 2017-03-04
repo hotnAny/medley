2d4e9b37a2b04f86ba626802d9a5d35143ad100f
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-03-04 11:47:09 -0500

    added painting on objects

--- src/medley.input.paint.js
+++ src/medley.input.paint.js
@@ -2,17 +2,32 @@ var MEDLEY = MEDLEY || {};
 
 MEDLEY.PaintInput = function(scene) {
     this._info = 'medley paint input';
+    this._radiusPaint = 1;
+    this._colorPaint = 0xfffa90;
+    this._isDown = false;
 }
 
 MEDLEY.PaintInput.prototype = {
     constructor: MEDLEY.PaintInput
 };
 
-MEDLEY.PaintInput.prototype.mousedown = function(e, hit){
+MEDLEY.PaintInput.prototype.mousedown = function(e, hit) {
+    this._isDown = true;
+    this._object = hit.object;
+    this._strokePoints = [];
 };
 
-MEDLEY.PaintInput.prototype.mousemove = function(e, hit){
+MEDLEY.PaintInput.prototype.mousemove = function(e) {
+    if (this._isDown) {
+        var hits = rayCast(e.clientX, e.clientY, [this._object]);
+        for (var hit of hits) {
+            this._strokePoints.push(hit.point);
+            addABall(hit.point, this._colorPaint, this._radiusPaint);
+        }
+    }
 };
 
-MEDLEY.PaintInput.prototype.mouseup = function(e, hit){
+MEDLEY.PaintInput.prototype.mouseup = function(e) {
+    this._isDown = false;
+    removeBalls();
 };
