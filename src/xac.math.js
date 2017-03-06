7febb3290fb60d7b317c1976b37e4c5552a63c6f
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-03-06 12:30:35 -0500

    added two useful xac libs

---
+++ src/xac.math.js
@@ -0,0 +1,249 @@
+// .................................................................
+//
+//  math library v0.1
+//
+//  by xiangchen@acm.org, 03/2017
+//
+// .................................................................
+
+//
+//  extensions for numeric.js
+//
+
+// compute the Frobenius norm of a matrix
+numeric.fnorm = function(matrix) {
+    var sum = 0;
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[i].length; j++) {
+            sum += Math.pow(matrix[i][j], 2);
+        }
+    }
+    return Math.sqrt(sum);
+}
+
+// print a matrix
+numeric.print = function(matrix) {
+    var strMatrix = ""
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[i].length; j++) {
+            strMatrix += parseFloat(matrix[i][j]).toFixed(4) + ' ';
+        }
+        strMatrix += '\n'
+            // strMatrix += matrix[i] + '\n';
+    }
+    console.log(strMatrix);
+}
+
+// times a matrix (including vector) by a scalar
+numeric.times = function(matrix, scalar) {
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[i].length; j++) {
+            matrix[i][j] *= scalar;
+        }
+    }
+    return matrix;
+}
+
+numeric.fromBlocks = function(blocks) {
+    var dim = numeric.dim(blocks).slice(0, 2);
+    var dimBlock = numeric.dim(blocks[0][0]).slice(0, 2);
+    var x = XAC.initMDArray([dim[0] * dimBlock[0], dim[1] * dimBlock[1]], 0);
+    for (var i = 0; i < dim[0]; i++) {
+        for (var j = 0; j < dim[1]; j++) {
+            var from = [i * dimBlock[0], j * dimBlock[1]];
+            var to = [from[0] + dimBlock[0] - 1, from[1] + dimBlock[1] - 1];
+            x = numeric.setBlock(x, from, to, blocks[i][j]);
+        }
+    }
+    // numeric.print(x)
+    return x;
+}
+
+//
+//  other math-related useful functions
+//
+
+//
+//  testing whether a triangle and a box intersect in 3D space,
+//	based on saparating axis theorem
+//	ref: http://fileadmin.cs.lth.se/cs/Personal/Tomas_Akenine-Moller/pubs/tribox.pdf
+//
+XAC.testTriBoxIntersection = function(va, vb, vc, nml, bbox) {
+    var minmax;
+
+    /* test the 3 box normals */
+    var boxNormals = [new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1)];
+    var boxMin = [bbox.min.x, bbox.min.y, bbox.min.z];
+    var boxMax = [bbox.max.x, bbox.max.y, bbox.max.z];
+
+    var project = function(points, axis) {
+    	var min = 1000;
+    	var max = -1000;
+
+    	for(var i=0; i<points.length; i++) {
+    		var val = axis.dot(points[i]);
+    		min = Math.min(min, val);
+    		max = Math.max(val, max);
+    	}
+
+    	return [min, max];
+    };
+
+
+    for (var i = 0; i < 3; i++) {
+        minmax = project([va, vb, vc], boxNormals[i]);
+        if (minmax[1] < boxMin[i] || minmax[0] > boxMax[i]) {
+            return false;
+        }
+    }
+
+
+    /* test the 1 triangle normal */
+    var boxVertices = new Array();
+    var xs = [bbox.min.x, bbox.max.x];
+    var ys = [bbox.min.y, bbox.max.y];
+    var zs = [bbox.min.z, bbox.max.z];
+
+    for (var ix = 0; ix < 2; ix++) {
+        for (var iy = 0; iy < 2; iy++) {
+            for (var iz = 0; iz < 2; iz++) {
+                boxVertices.push(new THREE.Vector3(xs[ix], ys[iy], zs[iz]));
+            }
+        }
+    }
+
+    var triOffset = nml.dot(va);
+    minmax = project(boxVertices, nml);
+    if (minmax[1] < triOffset || minmax[0] > triOffset) {
+        return false;
+    }
+
+
+    /* test the 9 edge cross products */
+    var triEdges = [new THREE.Vector3().subVectors(va, vb),
+        new THREE.Vector3().subVectors(vb, vc),
+        new THREE.Vector3().subVectors(vc, va)
+    ];
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var axis = new THREE.Vector3().crossVectors(triEdges[i], boxNormals[j]);
+            var boxMinmax = project(boxVertices, axis);
+            var triMinmax = project([va, vb, vc], axis);
+            if (boxMinmax[1] < triMinmax[0] || boxMinmax[0] > triMinmax[1]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+//
+//  get the max amongst all the input vars
+//
+XAC.max = function() {
+    var maxVal = Number.MIN_VALUE;
+    for (var i = 0; i < arguments.length; i++) {
+        maxVal = Math.max(maxVal, arguments[i]);
+    }
+    return maxVal;
+}
+
+XAC.min = function() {
+    var minVal = Number.MAX_VALUE;
+    for (var i = 0; i < arguments.length; i++) {
+        minVal = Math.min(minVal, arguments[i]);
+    }
+    return minVal;
+}
+
+//
+//  whether p1 and p2 are on the same side of the segment ab
+//
+XAC.onSameSide = function(p1, p2, a, b) {
+    var ab = b.clone().sub(a);
+    var cp1 = ab.clone().cross(p1.clone().sub(a));
+    var cp2 = ab.cross(p2.clone().sub(a));
+
+    var isSameSide = false;
+    // if at least one point is not on ab
+    if(cp1.length() != 0 || cp2.length != 0)
+         isSameSide = cp1.dot(cp2) > 0;
+    // if both points are on ab
+    else
+        isSameSide = true;
+
+    return isSameSide;
+}
+
+//
+//  whether v is in a triangle defined by va, vb and vc
+//
+XAC.isInTriangle = function(v, va, vb, vc) {
+    return XAC.onSameSide(v, va, vb, vc) &&
+        XAC.onSameSide(v, vb, va, vc) &&
+        XAC.onSameSide(v, vc, va, vb);
+}
+
+//
+//  force an input val to be between vmin and vmax
+//
+XAC.clamp = function(val, vmin, vmax) {
+	if(vmin > vmax) {
+		var vtmp = vmin;
+		vmin = vmax;
+		vmax = vtmp;
+	}
+	val = Math.max(vmin, val);
+	val = Math.min(val, vmax);
+	return val;
+}
+
+//
+// get triangle area
+//
+XAC.triangleArea = function(va, vb, vc) {
+	var ab = vb.clone().sub(va);
+	var ac = vc.clone().sub(va);
+
+	var x1 = ab.x,
+		x2 = ab.y,
+		x3 = ab.z,
+		y1 = ac.x,
+		y2 = ac.y,
+		y3 = ac.z;
+
+	return 0.5 * Math.sqrt(
+		Math.pow((x2 * y3 - x3 * y2), 2) +
+		Math.pow((x3 * y1 - x1 * y3), 2) +
+		Math.pow((x1 * y2 - x2 * y1), 2)
+	);
+}
+
+/*
+	assuming @param points is THREE.Vector3, or something that has x, y, z components
+	ref: http://stackoverflow.com/questions/10900141/fast-plane-fitting-to-many-points
+	svd related: http://www.mathworks.com/help/matlab/ref/svd.html
+*/
+XAC.findPlaneToFitPoints = function(points) {
+	var G = [];
+
+	for (var i = 0; i < points.length; i++) {
+		G.push([points[i].x, points[i].y, points[i].z, 1]);
+	}
+
+	var usv = numeric.svd(G);
+
+	var a = usv.V[0][3];
+	var b = usv.V[1][3];
+	var c = usv.V[2][3];
+	var d = usv.V[3][3];
+
+	return {
+		A: a,
+		B: b,
+		C: c,
+		D: d
+	};
+}
---
+++ src/xac.thing.js
@@ -0,0 +1,316 @@
+// ........................................................................................................
+//
+//  generating 3d things v0.2
+//
+//  by xiangchen@acm.org, 03/2017
+//
+// ........................................................................................................
+
+var XAC = XAC || {};
+
+var BACKGROUNDCOLOR = 0xF2F0F0;
+var GROUNDCOLOR = 0xF2F0F0;
+var GRIDCOLOR = 0x888888;
+
+var COLORNORMAL = 0xDB5B8A; // the normal color
+var COLORCONTRAST = 0xD1D6E7; // is the contrast of the COLORNORMAL
+var COLORHIGHLIGHT = 0xFFFA90;
+var COLORFOCUS = 0xE82C0C; // color to really draw users' focus
+
+XAC.MATERIALNORMAL = new THREE.MeshPhongMaterial({
+	color: COLORNORMAL,
+	transparent: true,
+	opacity: 0.75
+});
+
+XAC.MATERIALCONTRAST = new THREE.MeshPhongMaterial({
+	color: COLORCONTRAST,
+	transparent: true,
+	opacity: 0.75
+});
+
+XAC.MATERIALHIGHLIGHT = new THREE.MeshPhongMaterial({
+	color: COLORHIGHLIGHT,
+	transparent: true,
+	opacity: 0.95
+});
+
+XAC.MATERIALFOCUS = new THREE.MeshPhongMaterial({
+	color: COLORFOCUS,
+	transparent: true,
+	opacity: 0.95
+});
+
+XAC.Thing = function(m) {
+	this._g = undefined; // the original geometry, always!
+	this._m = m;
+	this._opacity = 1;
+	this._weight = undefined;
+};
+
+XAC.Thing.prototype = {
+	constructor: XAC.Thing,
+
+	get g() {
+		return this._g;
+	},
+
+	get gt() {
+		this._m.updateMatrixWorld();
+		var gTransformed = this._g.clone();
+		gTransformed.applyMatrix(this._m.matrixWorld);
+		return gTransformed;
+	},
+
+	get m() {
+		return this._m;
+	},
+
+	unitTest: function() {
+		log(this.g);
+	}
+}
+
+//
+//	line
+//
+XAC.Line = function(p1, p2, clr) {
+	this._g = new THREE.Geometry();
+	this._g.vertices.push(p1);
+	this._g.vertices.push(p2);
+	clr = clr == undefined ? 0x000000 : clr;
+	var mat = new THREE.LineBasicMaterial({
+		color: clr
+	});
+	this._m = new THREE.Line(this._g, mat);
+};
+XAC.Line.prototype = Object.create(XAC.Thing.prototype);
+
+//
+//	thick line
+//
+XAC.ThickLine = function(p1, p2, r, mat) {
+	var h = p1.distanceTo(p2);
+	this._line = new XAC.Cylinder(r, h, mat, true);
+	this._g = this._line.g;
+	this._m = this._line.m;
+
+	this._dir = p2.clone().sub(p1);
+	XAC.rotateObjTo(this._m, this._dir);
+	var ctr = p1.clone().add(p2).multiplyScalar(0.5);
+	this._m.position.copy(ctr);
+
+	this._r = r;
+}
+XAC.ThickLine.prototype = Object.create(XAC.Thing.prototype);
+XAC.ThickLine.prototype.update = function(p1, p2, r) {
+	var h = p1.distanceTo(p2);
+	this._line.update(r, h, undefined, true);
+
+	XAC.rotateObjTo(this._m, this._dir, true);
+	this._dir = p2.clone().sub(p1);
+	XAC.rotateObjTo(this._m, this._dir);
+
+	var ctr = p1.clone().add(p2).multiplyScalar(0.5);
+	this._m.position.copy(ctr);
+}
+XAC.ThickLine.prototype.updateEfficiently = function(p1, p2, r) {
+	var dir = p2.clone().sub(p1);
+	if (dir.length() > XAC.EPSILON && this._dir.length() > XAC.EPSILON) {
+		// rotate back
+		XAC.rotateObjTo(this._m, this._dir, true);
+
+		// scale
+		var yScale = dir.length() / this._dir.length();
+		var xzScale = (r.r1 + r.r2) / (this._r.r1 + this._r.r2);
+		scaleAroundVector(this._m, xzScale, new THREE.Vector3(0, 1, 0));
+		scaleAlongVector(this._m, yScale, new THREE.Vector3(0, 1, 0));
+
+		// rotate
+		XAC.rotateObjTo(this._m, dir);
+
+		this._r = r;
+		this._dir = dir;
+	}
+
+	// reposition
+	var ctr = p1.clone().add(p2).multiplyScalar(0.5);
+	this._m.position.copy(ctr);
+}
+
+//
+//	plane
+//
+XAC.Plane = function(w, l, mat) {
+	this._g = new THREE.CubeGeometry(w, 1, l);
+	this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() :
+		mat.clone());
+}
+XAC.Plane.prototype = Object.create(XAC.Thing.prototype);
+XAC.Plane.prototype.fitTo = function(p, a, b, c) {
+	rotateObjTo(this._m, new THREE.Vector3(a, b, c));
+	this._m.position.copy(p);
+}
+
+//
+//	sphere
+//
+XAC.Sphere = function(r, mat, highFi) {
+	this._r = r;
+	this._highFi = highFi;
+	this._g = this._highFi == true ? new THREE.SphereBufferGeometry(r, 32, 32) : new THREE
+		.SphereBufferGeometry(r, 8, 8);
+	this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() :
+		mat.clone());
+};
+XAC.Sphere.prototype = Object.create(XAC.Thing.prototype);
+XAC.Sphere.prototype.update = function(r, ctr) {
+	if (r != undefined)
+		this._m.geometry = this._highFi == true ? new THREE.SphereBufferGeometry(r, 32, 32) :
+		new THREE.SphereBufferGeometry(r, 8, 8);
+	if (ctr != undefined)
+		this._m.position.copy(ctr);
+}
+
+//
+// cylinder
+//
+XAC.Cylinder = function(r, h, mat, openEnded) {
+	this.update(r, h, mat, openEnded)
+}
+XAC.Cylinder.prototype = Object.create(XAC.Thing.prototype);
+XAC.Cylinder.prototype.update = function(r, h, mat, openEnded) {
+	this._h = h;
+	if (r.r1 != undefined && r.r2 != undefined) {
+		this._r1 = r.r1;
+		this._r2 = r.r2;
+	} else {
+		this._r1 = this._r2 = r;
+	}
+
+	// TODO: make sure the radius segment is enough for visualization
+	this._g = new THREE.CylinderBufferGeometry(this._r1, this._r2, this._h, 8, 1,
+		openEnded);
+	if (this._m == undefined) {
+		this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() :
+			mat.clone());
+	} else {
+		this._m.geometry = this._g;
+	}
+};
+
+//
+//	box
+//
+XAC.Box = function(w, t, l, mat) {
+	this._material = mat;
+	this.update(w, t, l, this._material);
+}
+XAC.Box.prototype = Object.create(XAC.Thing.prototype);
+XAC.Box.prototype.update = function(w, t, l) {
+	this._g = new THREE.CubeGeometry(w, t, l);
+	this._m = new THREE.Mesh(this._g,
+		this._material == undefined ? MATERIALNORMAL.clone() : this._material.clone()
+	);
+}
+
+//
+// 	polyhedron
+//	@param vertices - an array of THREE.Vector3
+//	@param faces - which vertices each face corresponds to
+//
+XAC.Polygon = function(vertices, faces, mat) {
+	// var arrVertices = [];
+	// for (var i = 0; i < vertices.length; i++) {
+	// 	arrVertices = arrVertices.concat(vertices[i].toArray());
+	// }
+
+	// this._g = new THREE.PolyhedronGeometry(arrVertices, faces, 1)
+	this._g = new THREE.Geometry();
+	this._g.vertices = vertices;
+	this._g.faces = faces;
+	this._m = new THREE.Mesh(this._g, mat == undefined ? XAC.MATERIALNORMAL.clone() :
+		mat.clone());
+}
+XAC.Polygon.prototype = Object.create(XAC.Thing.prototype);
+
+_balls = [];
+
+function addABall(pt, clr, radius) {
+	clr = clr == undefined ? 0xff0000 : clr;
+	radius = radius == undefined ? 1 : radius;
+
+	var geometry = new THREE.SphereGeometry(radius, 10, 10);
+	var material = new THREE.MeshBasicMaterial({
+		color: clr
+	});
+	var ball = new THREE.Mesh(geometry, material);
+	ball.position.set(pt.x, pt.y, pt.z);
+
+	_balls.push(ball);
+	XAC.scene.add(ball);
+
+	return ball;
+}
+
+function removeBalls() {
+	for (var i = _balls.length - 1; i >= 0; i--) {
+		XAC.scene.remove(_balls[i]);
+	}
+}
+
+function addATriangle(v1, v2, v3, clr) {
+	var vs = [v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z];
+	var fs = new THREE.Face3(0, 1, 2);
+
+	var geometry = new THREE.Geometry(); //PolyhedronGeometry(vs, fs, 1, 1);
+	geometry.vertices.push(v1);
+	geometry.vertices.push(v2);
+	geometry.vertices.push(v3);
+	geometry.faces.push(new THREE.Face3(0, 1, 2));
+	var material = new THREE.MeshBasicMaterial({
+		color: clr,
+		transparent: true,
+		opacity: 0.5
+	});
+	var tri = new THREE.Mesh(geometry, material);
+	tri.material.side = THREE.DoubleSide;
+
+	XAC.scene.add(tri);
+
+	return tri;
+}
+
+function addAnArrow(v1, dir, len, clr) {
+	var flipped = len < 0;
+
+	var rArrow = 0.025 * len;
+	var lArrow = len == undefined ? 100 : Math.abs(len);
+
+	var mat = clr == undefined ? MATERIALFOCUS : new THREE.MeshBasicMaterial({
+		color: clr,
+		transparent: true,
+		opacity: 1.0
+	});
+
+	var bodyArrow = new XAC.Cylinder(rArrow, lArrow, mat).m;
+
+	var rArrowHead = rArrow * 5;
+	var headArrow = new XAC.Cylinder({
+		r1: 0,
+		r2: rArrowHead
+	}, rArrowHead * 2, mat).m;
+	headArrow.position.add(new THREE.Vector3(0, 1, 0).multiplyScalar(lArrow * 0.5 + rArrowHead));
+
+	var arrow = new THREE.Object3D();
+	arrow.add(bodyArrow);
+	arrow.add(headArrow);
+
+	rotateObjTo(arrow, dir.clone().normalize().multiplyScalar(flipped == true ? -1 : 1));
+	arrow.position.copy(v1.clone().add(
+		dir.clone().normalize().multiplyScalar(lArrow * 0.5 + (flipped == true ?
+			rArrowHead * 2 : 0))));
+
+	XAC.scene.add(arrow);
+	return arrow;
+}
