549cd56378fc4728b46177725d3e4102e9bf91f0
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-04-27 16:52:06 -0400

    code review

--- src/medley.fabrication.js
+++ src/medley.fabrication.js
@@ -1,8 +1,8 @@
 //	........................................................................................................
 //
 //  medley fabrication
-//      - smoothen geometry to fit material's bend radius
-//
+//      - fit1dBendRadius: smoothen 1d (wire/tube/etc.) geometry to fit material's bend radius
+//      - make1dFabricatable: solve fabrication-related problems for 1d (wire/tube/etc.) embeddable geometry
 //  by xiangchen@acm.org, 04/2017
 //
 //	........................................................................................................
@@ -10,10 +10,12 @@
 var MEDLEY = MEDLEY || {};
 
 //
-//
+//  smoothen 1d (wire/tube/etc.) embeddable geometry to fit material's bend radius
+//  - info: information of user input
+//  - points: a set of points representing the polyline underneath the geometry
+//  - r: material's bend radius
 //
 MEDLEY.fit1dBendRadius = function(info, points, r) {
-    var minRadiusPrev;
     var eps = 10e-4;
     var lambda = 0.382;
     var nitr = 0;
@@ -27,40 +29,45 @@ MEDLEY.fit1dBendRadius = function(info, points, r) {
             var v1 = points[i];
             var v2 = points[i + 1];
 
+            // find the center from v0, v2, assuming the bend radius
             var t = v1.clone().sub(v0).cross(v2.clone().sub(v1));
             var u = v2.clone().sub(v0);
             var w = t.cross(u).normalize();
             var height = Math.sqrt(Math.pow(r, 2) - Math.pow(u.length() / 2, 2));
             var c = v0.clone().add(u.divideScalar(2)).add(w.clone().multiplyScalar(height));
 
-            if(v1.distanceTo(c) <= r + eps) {
+            // if v1 is in this bend-radius-circle, it means a v0-v1-v2 circle has a >r radius
+            if (v1.distanceTo(c) <= r + eps) {
                 pointsNew.push(v1);
                 continue;
             }
 
             noUpdate = false;
 
+            // where v1 should be to fit the bend radius
             var v1target = c.clone().sub(w.clone().multiplyScalar(r));
 
+            // interpret v1 to the target position
             var lambda1 = lambda * r / v1.distanceTo(c);
             var v1intrpr = v1.clone().multiplyScalar(lambda1).add(v1target.multiplyScalar(1 - lambda1));
             pointsNew.push(v1intrpr);
         }
         pointsNew.push(points.last());
 
-        if(noUpdate) break;
+        if (noUpdate) break;
 
         for (var i = 1; i < points.length - 1; i++) {
             points[i].copy(pointsNew[i]);
         }
     }
 
-    log('fit after ' + nitr + ' iterations');
+    console.info('fit after ' + nitr + ' iterations');
 }
 
 
 //
-//
+//  solve fabrication-related problems for 1d (wire/tube/etc.) embeddable geometry
+//  - embeddable: the embeddable that needs to be embedded
 //
 MEDLEY.make1dFabricatable = function(embeddable) {
     var mat = XAC.MATERIALCONTRAST.clone();
@@ -98,12 +105,6 @@ MEDLEY.make1dFabricatable = function(embeddable) {
         }
 
         c.z = k * c.x + b;
-
-        // XXX
-        // if (isNaN(c.x)) {
-        //     log(_b * _b - 4 * _a * _c)
-        //     log(['v'].concat(v.toArray()));
-        // }
 
         return c;
     }
@@ -173,38 +174,23 @@ MEDLEY.make1dFabricatable = function(embeddable) {
     }
     ymax += MEDLEY._layerHeight;
 
-    // XXX
-    // var plane = new XAC.Plane(100, 100, mat);
-    // plane.fitTo(ymaxPoint, 0, 1, 0);
-    // XAC.scene.add(plane.m)
-
-    // _balls.remove(addABall(ymaxPoint, 0x00ff00, 1))
-    // XXX
-
     //
     // compute tangents at the twoend points
     //
     var p0 = embeddable.points[0].clone().applyMatrix4(embeddable._meshes.matrixWorld);
     var p1 = embeddable.points[1].clone().applyMatrix4(embeddable._meshes.matrixWorld);
     var tangent0 = new THREE.Vector3().subVectors(p0, p1).normalize();
-    // addAnArrow(p0, tangent0, 10, 0xff0000);
     var info0 = __findOpening(p0, tangent0, ymax, embeddable.bendRadius);
-    // _balls.remove(addABall(info0.q, 0xff0000, 0.5))
 
     var pn = embeddable.points.last().clone().applyMatrix4(embeddable._meshes.matrixWorld);
     var pn1 = embeddable.points.lastBut(1).clone().applyMatrix4(embeddable._meshes.matrixWorld);
     var tangentn = new THREE.Vector3().subVectors(pn, pn1).normalize();
-    // addAnArrow(pn1, tangentn, 10, 0xff0000);
     var infon = __findOpening(pn, tangentn, ymax, embeddable.bendRadius);
-    // _balls.remove(addABall(infon.q, 0xff0000, 0.5))
 
     var info = info0.angle < infon.angle ? info0 : infon;
-    // log(q.toArray())
-    // _balls.remove(addABall(info.q, 0x00ff00, 1))
 
     var step = 5 * Math.PI / 180;
     var angleBetween = Math.acos(1 - info.angle);
-    // log(angleBetween)
     var axis = info.p.clone().sub(info.c).cross(info.q.clone().sub(info.c)).normalize();
     embeddable.extra = new THREE.Object3D();
     var matTunnel = XAC.MATERIALFOCUS.clone();
@@ -224,7 +210,6 @@ MEDLEY.make1dFabricatable = function(embeddable) {
         var segment = new XAC.ThickLine(p0, p1, embeddable._matobj.radius, matTunnel);
         if (segment.m.geometry.isBufferGeometry)
             segment.m.geometry = new THREE.Geometry().fromBufferGeometry(segment.m.geometry);
-        // _balls.remove(addABall(p, 0xff00ff, 0.5))
         embeddable.extra.add(segment.m);
 
         joint = new XAC.Sphere(embeddable._matobj.radius, matTunnel, false);
