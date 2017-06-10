54356b8055122535fde63e3a57d0b8159c517876
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-06-10 11:58:36 -0400

    added insertion sort

--- src/xac.ds.js
+++ src/xac.ds.js
@@ -8,8 +8,6 @@
 
 var XAC = XAC || {};
 
-// 
-
 XAC.Sortable = function (algorithm) {
     this._algorithm = algorithm;
     switch (this._algorithm) {
@@ -26,6 +24,9 @@ XAC.Sortable = function (algorithm) {
     }
 
 };
+
+XAC.Sortable.INSERTION = 0;
+XAC.Sortable.BST = 1;
 
 XAC.Sortable.prototype = {
     constructor: XAC.Sortable
@@ -49,6 +50,26 @@ XAC.Sortable.prototype.insert = function (value) {
                 };
             }
             break;
+        case XAC.Sortable.INSERTION:
+            var idx = -1;
+            for (var i = 0; i < this._elms.length; i++) {
+                if (this._elms[i] > value) {
+                    this._elms.insert(value, i);
+                    idx = i;
+                    break;
+                }
+            }
+            if (idx < 0) this._elms.push(value);
+            return idx < 0 ? this._elms.length - 1 : idx;
+    }
+}
+
+XAC.Sortable.prototype.getSortedList = function() {
+    switch (this._algorithm) {
+        case XAC.Sortable.BST:
+            return [];
+        case XAC.Sortable.INSERTION:
+            return this._elms;
     }
 }
 
@@ -60,7 +81,7 @@ XAC.Sortable.prototype.print = function () {
         case XAC.Sortable.BST:
             var __printBST = function (node, str) {
                 var str = str || '';
-                if(node == undefined) return str;
+                if (node == undefined) return str;
                 str = __printBST(node.left, str);
                 str += node.value + ' ';
                 str = __printBST(node.right, str);
@@ -69,7 +90,17 @@ XAC.Sortable.prototype.print = function () {
             console.log(__printBST(this._root));
 
             break;
+        case XAC.Sortable.INSERTION:
+            console.log(this._elms);
+            break;
     }
+}
+
+//
+//
+//
+XAC.Sortable.prototype.getNode = function (value) {
+    return this._traverse(value, this._root);
 }
 
 //
