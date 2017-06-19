d666767f6c6bc25bc4381999b72a3caaf4397c09
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-06-19 10:34:43 -0400

    updated sandbox

--- src/medley.sandbox.js
+++ src/medley.sandbox.js
@@ -2,19 +2,17 @@ var MEDLEY = MEDLEY || {};
 
 var _fitInfo;
 
-MEDLEY._layerHeight = 0.2;
-
 // XXX
 MEDLEY._matobjSelected = {
     radius: 0.75, // mm
     dim: 0,
     thickness: 1, // mm
     bendRadius: 50, // mm
-    // meshPath: 'embeddables/penny.stl'
+    meshPath: 'embeddables/penny.stl'
     // meshPath: 'embeddables/screw_phillips.stl'
     // meshPath: 'embeddables/screw_fh1.5in.stl'
     // meshPath: 'things/spoon.stl'
-    meshPath: 'embeddables/rod1.stl'
+    // meshPath: 'embeddables/rod1.stl'
 };
 
 
@@ -38,9 +36,9 @@ $(document).ready(function () {
     XAC.on(XAC.ENTER, function () {
         var embeddable = MEDLEY.embeddables.last();
 
-        // MEDLEY.findExternalInsertion(embeddable);
+        MEDLEY.findPostPrintInsertion(embeddable);
 
-        MEDLEY.find0dInternalInsertion(embeddable);
+        // MEDLEY.find0dInternalInsertion(embeddable);
     });
 
     XAC.on('S', function () {
@@ -97,74 +95,82 @@ $(document).ready(function () {
     // log(sortable.insert(12.45658));
     // sortable.print(); //);
 
-    var maxHeight = 5;
-    var bboxes = [];
-    var __insertBox = function (x0, x1, y0, y1, z0, z1, bboxes) {
-        bboxes.push({
-            min: {
-                x: x0,
-                y: y0,
-                z: z0
-            },
-            max: {
-                x: x1,
-                y: y1,
-                z: z1
-            }
-        });
-    }
-
-    __insertBox(0, 3, 0, 1, 0, 2, bboxes);
-    __insertBox(-1, 1, 1, 2, -1, 1, bboxes);
-    __insertBox(-2, 2, 2, 3, -2, 3, bboxes);
-    __insertBox(-1, 1, 3, 4, 0, 2, bboxes);
-    __insertBox(3, 4, 4, 5, -2, 0, bboxes);
+    // var maxHeight = 5;
+    // var bboxes = [];
+    // var __insertBox = function (x0, x1, y0, y1, z0, z1, bboxes) {
+    //     bboxes.push({
+    //         min: {
+    //             x: x0,
+    //             y: y0,
+    //             z: z0
+    //         },
+    //         max: {
+    //             x: x1,
+    //             y: y1,
+    //             z: z1
+    //         }
+    //     });
+    // }
 
     
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
+    // MEDLEY._matobjs.push(new MEDLEY.MatObj());
 });
 
 //
-//
+//  get ready for fabrication
 //
 MEDLEY.getFabReady = function (embeddable) {
-    // merged and extra are the ultimate 'things' that need to be removed from the object for insertion
-    var merged, extra;
+    // cutoff and extra are the ultimate 'things' that need to be removed from the object for insertion
+    var cutoff, extra;
     var material = XAC.MATERIALNORMAL.clone();
     switch (embeddable._dim) {
         case 0:
+            cutoff = embeddable._cutoff;
             break;
         case 1:
-            merged = embeddable._extrudedSegments.m;
+            cutoff = embeddable._extrudedSegments.m;
             extra = embeddable._extraSegments == undefined ? undefined : embeddable._extraSegments.m;
             break;
         case 2:
         case 3:
             for (mesh of embeddable._meshes.children) {
                 // var meshbsp = new ThreeBSP(mesh);
-                if (merged == undefined) {
-                    merged = new THREE.Mesh(gettg(mesh), material);
+                if (cutoff == undefined) {
+                    cutoff = new THREE.Mesh(gettg(mesh), material);
                 } else {
-                    merged.geometry.merge(gettg(mesh));
+                    cutoff.geometry.merge(gettg(mesh));
                 }
             }
-
-            MEDLEY.fixFaces(merged);
+            MEDLEY.fixFaces(cutoff);
             break;
         default:
             break;
     }
 
-
     XAC.scene.remove(MEDLEY.everything);
-    var csgObject = new ThreeBSP(embeddable._object);
-    var csgEmbeddable = new ThreeBSP(merged);
-    csgObject = csgObject.subtract(csgEmbeddable);
+    // var csgObject = new ThreeBSP(embeddable._object);
+    // var csgEmbeddable = new ThreeBSP(cutoff);
+    // csgObject = csgObject.subtract(csgEmbeddable);
+    var meshReady = XAC.subtract(embeddable._object, cutoff);
     if (extra != undefined) {
         XAC.scene.remove(extra);
-        var csgExtra = new ThreeBSP(extra);
-        csgObject = csgObject.subtract(csgExtra);
+        // var csgExtra = new ThreeBSP(extra);
+        // csgObject = csgObject.subtract(csgExtra);
+        meshReady = XAC.subtract(meshReady, extra);
     }
-    var meshReady = csgObject.toMesh(material);
+    // meshReady = csgObject.toMesh(material);
     XAC.scene.add(meshReady);
     return meshReady;
 }
@@ -202,138 +208,3 @@ MEDLEY.getFabReady = function (embeddable) {
 //         }, 100);
 //     }
 // }
-
-//
-//
-//
-MEDLEY.find0dInternalInsertion = function (embeddable) {
-    var dirInsertion = new THREE.Vector3(0, 1, 0).normalize();
-    var dh = MEDLEY._layerHeight;
-    var vertices = embeddable._mesh.geometry.vertices.clone();
-
-    // temporarilly rotate the object along the insertion direction
-    var mr = new THREE.Matrix4();
-    var yUp = new THREE.Vector3(0, 1, 0);
-    var angleToRotate = -yUp.angleTo(dirInsertion);
-    var axisToRotate = new THREE.Vector3().crossVectors(yUp, dirInsertion).normalize();
-    mr.makeRotationAxis(axisToRotate, angleToRotate);
-
-    // find the bounding box of the rotated object
-    // var boundingInfo = getBoundingBoxEverything(embeddable._mesh);
-    var tg = embeddable._mesh.geometry.clone();
-    tg.applyMatrix(mr);
-    tg.computeBoundingBox();
-    var minHeight = tg.boundingBox.min.y;
-    var maxHeight = tg.boundingBox.max.y;
-    _balls.remove(addABall(tg.boundingBox.min, 0x00ff00, 1));
-    _balls.remove(addABall(tg.boundingBox.max, 0x0000ff, 1));
-    var nlevels = ((maxHeight - minHeight) / dh + 1) | 0;
-
-    log(nlevels)
-
-    // scan the object's vertices, put them into different layers
-    var bboxes = [];
-    for (var i = 0; i < nlevels; i++) {
-        bboxes.push({
-            min: new THREE.Vector3(Number.MAX_VALUE, minHeight + i * dh, Number.MAX_VALUE),
-            max: new THREE.Vector3(Number.MIN_VALUE, minHeight + (i + 1) * dh, Number.MIN_VALUE)
-        });
-    }
-
-    var __updateBbox = function (bboxes, idx, v) {
-        bboxes[idx].min.x = Math.min(bboxes[idx].min.x, v.x);
-        bboxes[idx].min.z = Math.min(bboxes[idx].min.z, v.z);
-        bboxes[idx].max.x = Math.max(bboxes[idx].max.x, v.x);
-        bboxes[idx].max.z = Math.max(bboxes[idx].max.z, v.z);
-    }
-
-    for (v of vertices) {
-        var u = v.clone().applyMatrix4(mr);
-        var idx = (nlevels * (u.y - minHeight) / (maxHeight - minHeight)) | 0;
-        idx = Math.min(nlevels - 1, idx);
-        __updateBbox(bboxes, idx, u);
-        // var _color = 0x0000ff * idx;
-        // _balls.remove(addABall(u, _color, 0.25));
-    }
-
-    // [debug]
-    // for (bbox of bboxes) {
-    //     var w = bbox.max.x - bbox.min.x;
-    //     var t = bbox.max.y - bbox.min.y;
-    //     var l = bbox.max.z - bbox.min.z;
-    //     var box = new XAC.Box(w, t, l).m;
-    //     box.position.copy(new THREE.Vector3().addVectors(bbox.max, bbox.min).multiplyScalar(0.5));
-    //     XAC.scene.add(box);
-    // }
-
-    // var __getIntersectionBox = function (box1, box2) {
-    //     // var w = Math.min(box1.max.x, box2.max.x) - Math.max(box1.min.x, box2.min.x);
-    //     // var l = Math.min(box1.max.z, box2.max.z) - Math.max(box1.min.z, box2.min.z);
-    //     var box = {
-    //         min: {
-    //             x: Math.max(box1.min.x, box2.min.x),
-    //             z: Math.max(box1.min.z, box2.min.z)
-    //         },
-    //         max: {
-    //             x: Math.min(box1.max.x, box2.max.x),
-    //             z: Math.min(box1.max.z, box2.max.z)
-    //         }
-    //     };
-
-    //     // var w = box.max.x - box.min.x;
-    //     // var l = box.max.z - box.min.z;
-
-    //     return box;
-    // };
-
-
-    var __findUnionArea = function (start, end, xlist, boxes) {
-        if (boxes.length == 0) return 0;
-        var area = 0;
-        for (var i = start; i < end; i++) {
-            var xmin = xlist[i],
-                xmax = xlist[i + 1];
-            var zmin = Number.MAX_VALUE;
-            var zmax = -Number.MAX_VALUE;
-            for (box of boxes) {
-                if (box.min.x <= xmin && box.max.x >= xmax) {
-                    zmin = Math.min(zmin, box.min.z);
-                    zmax = Math.max(zmax, box.max.z);
-                }
-            }
-
-            if (zmin == Number.MAX_VALUE || zmax == Number.MIN_VALUE) area += 0;
-            else area += (xmax - xmin) * (zmax - zmin);
-        }
-        return area;
-    }
-
-    var sumVols = 0;
-    var xvalues = [];
-    var bboxesPrev = [];
-    var schedule = new XAC.Sortable(XAC.Sortable.INSERTION);
-    for (var i = 0; i < bboxes.length; i++) {
-        var bbox = bboxes[i];
-        var start = schedule.insert(bbox.min.x);
-        var end = schedule.insert(bbox.max.x);
-        var xlist = schedule.getSortedList();
-
-        var area = __findUnionArea(start, end, xlist, bboxesPrev);
-        bboxesPrev.push(bbox);
-        var areaNew = __findUnionArea(start, end, xlist, bboxesPrev);
-
-        areaNew -= area;
-        var vol = areaNew * (maxHeight - bbox.min.y);
-        log(vol)
-        sumVols += vol;
-        // xvalues = xvaluesNew;
-    }
-
-    log(sumVols)
-
-    // rotate the object back
-    // rotateGeoTo(obj, dirInsertion);
-    rotateGeoTo(embeddable._mesh, dirInsertion, true);
-
-    // generate cut-off part and align it with the object
-}
