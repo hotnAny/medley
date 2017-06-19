9b48a4f6d0a14563aa5c2d763a4041918555b0ba
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-06-19 10:22:25 -0400

    load dialog and panel both from templates

--- src/medley.matobj.js
+++ src/medley.matobj.js
@@ -8,7 +8,12 @@
 
 var MEDLEY = MEDLEY || {};
 
-MEDLEY.MatObj = function () {
+//
+//
+//
+MEDLEY.MatObj = function(matobjs) {
+    this._matobjs = matobjs;
+
     // [debug]
     this._name = 'Banana';
     this._imgSrc = MEDLEY.ASSETDIR + '/359-banana.png';
@@ -16,9 +21,11 @@ MEDLEY.MatObj = function () {
     this._dim = 1;
 
     this._properties = {};
-    this._properties[MEDLEY.HEATCONDUCTIVITY] = 5;
-    this._properties[MEDLEY.TENSILESTRENGTH] = 4;
-    this._properties[MEDLEY.SOFTNESS] = 0;
+    // this._properties[MEDLEY.HEATCONDUCTIVITY] = 5;
+    // this._properties[MEDLEY.TENSILESTRENGTH] = 4;
+    // this._properties[MEDLEY.SOFTNESS] = 0;
+
+    this._lbStars = {};
 };
 
 MEDLEY.MatObj.prototype = {
@@ -28,11 +35,10 @@ MEDLEY.MatObj.prototype = {
 //
 //
 //
-MEDLEY.MatObj.prototype.getInfoCard = function (parent) {
+MEDLEY.MatObj.prototype.getInfoCard = function(parent) {
     this._cardParent = parent;
 
     var card = $('<div class="w3-panel w3-card"></div>');
-    // card.css('margin', '5px');
     var tblCard = $('<table border="0"></table>');
     tblCard.css('width', '100%');
     var trCard = $('<tr></tr>');
@@ -45,41 +51,33 @@ MEDLEY.MatObj.prototype.getInfoCard = function (parent) {
     trCard.append(tdThumbnail);
 
     var tdInfo = $('<td></td>');
-    tdInfo.css('font-size', 'smaller');
+    tdInfo.css('vertical-align', 'top');
+    tdInfo.css('font-size', 'x-small');
+    tdInfo.css('width', '100%');
     tdInfo.append($('<b>' + this._name + '</b>'));
     var divProperties = $('<div></div>');
-    divProperties.css('font-size', 'x-small');
+    // divProperties.css('font-size', 'x-small');
     var properties = this._selectProperties();
+    var nproperties = 0;
     for (propName in properties) {
+        if (nproperties++ >= MEDLEY.MAXNUMPROPERTIESONCARD) {
+            divProperties.append('...');
+            break;
+        }
         var nstars = properties[propName];
         var strStars = '';
         for (var i = 0; i < nstars; i++) strStars += MEDLEY.CODEBLACKSTAR;
-        for (var i = 0; i < 5 - nstars; i++) strStars += MEDLEY.CODEWHITESTAR;
-        divProperties.append(propName + ': ' + strStars + '<br/>');
+        for (var i = 0; i < MEDLEY.NUMSTARS - nstars; i++) strStars += MEDLEY.CODEWHITESTAR;
+        divProperties.append(propName.replace('_', ' ') + ': ' + strStars + '<br/>');
     }
     tdInfo.append(divProperties);
 
-    var btnMore = $('<a href="..">More</a>')
-    btnMore.css('font-size', 'x-small');
+    var btnMore = $('<a href="">Edit</a>')
+        // btnMore.css('font-size', 'x-small');
     btnMore.css('float', 'right');
-    btnMore.click(function (e) {
+    btnMore.click(function(e) {
         e.preventDefault();
-        var dialog = this.getDialog();
-        dialog.dialog({
-            width: MEDLEY.WIDTHDIALOG,
-            // position: {
-            //     my: "left",
-            //     at: "center",
-            //     of: window
-            // },
-            open: function (e) {
-                //
-            }.bind(this),
-            close: function (e) {
-                this._updateCard();
-                this._dialogBody.remove();
-            }.bind(this)
-        });
+        this.openDialog();
         e.stopPropagation();
     }.bind(this));
     tdInfo.append(btnMore);
@@ -89,9 +87,10 @@ MEDLEY.MatObj.prototype.getInfoCard = function (parent) {
     tblCard.append(trCard);
     card.append(tblCard);
 
-    card.click(function (e) {
+    card.click(function(e) {
+        var selected = card.hasClass('ui-state-highlight');
         $('.w3-panel.w3-card').removeClass('ui-state-highlight');
-        card.addClass('ui-state-highlight');
+        if (!selected) card.addClass('ui-state-highlight');
     })
 
     this._card = card;
@@ -101,18 +100,18 @@ MEDLEY.MatObj.prototype.getInfoCard = function (parent) {
 //
 //
 //
-MEDLEY.MatObj.prototype._selectProperties = function (query) {
+MEDLEY.MatObj.prototype._selectProperties = function(query) {
     return this._properties;
 }
 
-MEDLEY.MatObj.prototype.getDialog = function () {
+MEDLEY.MatObj.prototype.getDialog = function() {
     var dialog = $('<div name="title" title="' + this._name + ' material properties">');
 
     var dialogBody = $('<div class="ui-widget"></div>');
-    dialogBody.click(function (e) {
+    dialogBody.click(function(e) {
         if (MEDLEY.MatObj.__editableText != undefined) {
             this._name = MEDLEY.MatObj.__editableText.val();
-            this.__makeNameLabel($('#divName'), this._name);
+            this._makeNameLabel($('#divName'), this._name);
             MEDLEY.MatObj.__editableText.remove();
             MEDLEY.MatObj.__editableText = undefined;
         }
@@ -126,12 +125,12 @@ MEDLEY.MatObj.prototype.getDialog = function () {
     selWorkability.append('<option value=1> 1 - wires, threads, strings, etc. </option>');
     selWorkability.append('<option value=2> 2 - sheets, panes, plates, etc. </option>');
     selWorkability.append('<option value=3> 3 - bars, blocks, lumps, etc. </option>');
-    selWorkability.change(function (event) {
-        this._dim = event.target.selectedIndex - 1;
+    selWorkability.change(function(e) {
+        this._dim = e.target.selectedIndex - 1;
     }.bind(this));
 
     // dropzone for 3d model files and thumbnails
-    var __isImage = function (str) {
+    var __isImage = function(str) {
         if (str.endsWith('png') || str.endsWith('jpg'))
             return true;
     }
@@ -142,13 +141,13 @@ MEDLEY.MatObj.prototype.getDialog = function () {
     divDropZone.css('border-width', '1px');
     divDropZone.css('line-height', '64px');
     divDropZone.css('color', '#888888');
-    divDropZone.on('dragover', function (e) {
+    divDropZone.on('dragover', function(e) {
         e.stopPropagation();
         e.preventDefault();
         e.dataTransfer = e.originalEvent.dataTransfer;
         e.dataTransfer.dropEffect = 'copy';
     });
-    divDropZone.on('drop', function (e) {
+    divDropZone.on('drop', function(e) {
         e.stopPropagation();
         e.preventDefault();
         e.dataTransfer = e.originalEvent.dataTransfer;
@@ -165,11 +164,29 @@ MEDLEY.MatObj.prototype.getDialog = function () {
         }
     }.bind(this));
 
-    dialogBody.load(MEDLEY.TABLEINFODIALOG, function (e) {
+    // material properties
+    var selMaterialProperty = $('<select id="selMaterialProperty"></select>');
+    selMaterialProperty.css('width', '100%');
+    for (var i = 0; i < MEDLEY.MATERIALPROPERTIES.length; i++) {
+        var property = MEDLEY.MATERIALPROPERTIES[i];
+        selMaterialProperty.append('<option value=' + i + '>' + property + '</option>');
+    }
+    selMaterialProperty.change(function(e) {
+        var idx = e.target.selectedIndex;
+        if (idx >= 0) {
+            var property = MEDLEY.MATERIALPROPERTIES[idx];
+            if (property.replace(' ', '_') in this._properties) return;
+            this._properties[property.replace(' ', '_')] = 0;
+            this._addPropertyRow(property);
+            this._updateCard();
+        }
+    }.bind(this));
+
+    dialogBody.load(MEDLEY.TABLEINFODIALOG, function(e) {
         $('#imgDialogThumbnail').attr('src', this._imgSrc);
         $('#imgDialogThumbnail').width(MEDLEY.WIDTHDIALOGTHUMBNAIL);
 
-        this.__makeNameLabel($('#divName'), this._name);
+        this._makeNameLabel($('#divName'), this._name);
 
         $('#divWorkability').append(selWorkability);
         if (this._dim != undefined)
@@ -177,29 +194,27 @@ MEDLEY.MatObj.prototype.getDialog = function () {
 
         $('#divDropzone').append(divDropZone);
 
+        $('#divProperties').append(selMaterialProperty);
+
+        for (property in this._properties) this._addPropertyRow(property);
+
+        $('#btnSave').button();
+        $('#btnSave').click(function(e) {
+            if (this._matobjs.indexOf(this) < 0) {
+                this._matobjs.push(this);
+                MEDLEY.showSearchResults(this);
+            }
+        }.bind(this));
+
     }.bind(this));
 
     this._dialogBody = dialogBody;
     dialog.append(dialogBody);
 
-    // name
-
-    // var divName = $('<div name="divName"></div>');
-    // __makeNameLabel(divName, this._name);
-    // dialog.append(divName);
-
-
-    // dialog.append('<h4 class="ui-widget">Workability</h4>');
-    // dialog.append('<br/>');
-
-
-
-
-
     return dialog;
 }
 
-MEDLEY.MatObj.prototype._updateCard = function () {
+MEDLEY.MatObj.prototype._updateCard = function() {
     if (this._cardParent != undefined) {
         if (this._card != undefined) {
             this._card.remove();
@@ -208,22 +223,22 @@ MEDLEY.MatObj.prototype._updateCard = function () {
     }
 }
 
-MEDLEY.MatObj.prototype.__makeNameLabel = function (div, name) {
+MEDLEY.MatObj.prototype._makeNameLabel = function(div, name) {
     var lbName = $('<label>' + name + '</label>');
     lbName.css('font-weight', 'bold');
     div.append(lbName);
     MEDLEY.MatObj.__instance = this;
-    lbName.click(function (e) {
+    lbName.click(function(e) {
         var lbHtml = $(this).html(); // notice "this" instead of a specific #myDiv
         MEDLEY.MatObj.__editableText = $('<input class="ui-widget" type="text" />');
         MEDLEY.MatObj.__editableText.css('font-weight', 'bold');
         MEDLEY.MatObj.__editableText.val(lbHtml);
-        MEDLEY.MatObj.__editableText.keyup(function (e) {
+        MEDLEY.MatObj.__editableText.keyup(function(e) {
             $('div[name="title"]').dialog('option', 'title', $(this).val() + ' material properties');
             MEDLEY.MatObj.__instance._name = $(this).val();
             MEDLEY.MatObj.__instance._updateCard();
         });
-        MEDLEY.MatObj.__editableText.click(function (e) {
+        MEDLEY.MatObj.__editableText.click(function(e) {
             e.stopPropagation();
         });
         $(this).replaceWith(MEDLEY.MatObj.__editableText);
@@ -231,4 +246,86 @@ MEDLEY.MatObj.prototype.__makeNameLabel = function (div, name) {
         e.stopPropagation();
         this.remove();
     });
+}
+
+MEDLEY.MatObj.prototype._getInteractiveStars = function(property) {
+    // this._lbStars[property] = this._lbStars[property] || [];
+    var __lbStars = [];
+    var divStars = $('<div></div>');
+    var value = this._properties[property]
+    for (var i = 0; i < MEDLEY.NUMSTARS; i++) {
+        var star = i < value ? MEDLEY.CODEBLACKSTAR : MEDLEY.CODEWHITESTAR;
+        var lbStar = $('<label>' + star + '</label>');
+        lbStar.attr('star', i + 1);
+        lbStar.attr('matprop', property);
+        lbStar.click(function(e) {
+            var value = $(e.target).attr('star');
+            var __lbStars = this._lbStars[$(e.target).attr('matprop')];
+            for (var j = 0; j < value; j++) __lbStars[j].html(MEDLEY.CODEBLACKSTAR);
+            for (var j = value; j < MEDLEY.NUMSTARS; j++) __lbStars[j].html(MEDLEY.CODEWHITESTAR);
+            this._properties[$(e.target).attr('matprop').replace(' ', '_')] = value;
+            this._updateCard();
+        }.bind(this));
+        divStars.append(lbStar);
+        __lbStars.push(lbStar);
+    }
+
+    this._lbStars[property] =  __lbStars;
+
+    return divStars;
+}
+
+MEDLEY.MatObj.prototype._addPropertyRow = function(property) {
+    var trProperty = $('<tr id="tr' + property.replace(' ', '_') + '"></tr>');
+
+    var tdName = $('<td width="60%"><div>' + property + '</div></td>');
+    tdName.css('padding-bottom', '0px');
+    trProperty.append(tdName);
+
+    var tdStars = $('<td></td>');
+    tdStars.css('padding-bottom', '0px');
+    tdStars.append(this._getInteractiveStars(property.replace(' ', '_')));
+    trProperty.append(tdStars);
+
+    var tdRemove = $('<td></td>');
+    tdRemove.css('padding-bottom', '0px');
+    tdRemove.css('vertical-align', 'middle');
+    var btnRemove = $('<a style="text-decoration:none" href="">' + MEDLEY.CODECROSS + '</a>')
+        // btnRemove.button();
+    btnRemove.attr('matprop', property.replace(' ', '_'));
+    btnRemove.click(function(e) {
+        e.preventDefault();
+        var _property = $(e.target).attr('matprop');
+        $('#tr' + _property).remove();
+        delete this._properties[_property];
+        this._updateCard();
+    }.bind(this));
+    tdRemove.append(btnRemove);
+    trProperty.append(tdRemove);
+
+    $('#tblProperties').append(trProperty);
+}
+
+MEDLEY.MatObj.prototype.openDialog = function() {
+    var dialog = this.getDialog();
+    dialog.dialog({
+        width: MEDLEY.WIDTHDIALOG,
+        height: MEDLEY.HEIGHTDIALOG,
+        // position: {
+        //     my: "left",
+        //     at: "center",
+        //     of: window
+        // },
+        open: function(e) {
+            //
+        }.bind(this),
+        close: function(e) {
+            this._updateCard();
+            this._dialogBody.remove();
+        }.bind(this)
+    });
+}
+
+MEDLEY.MatObj.prototype.saveToLibrary = function() {
+
 }
